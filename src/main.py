68b2dcda8f73aa7515de93379f544d699a714515
TCL <1620508360@qq.com>
2023-01-11 20:57:56 +0800

    chore: add time var and local variation

--- src/main.py
+++ src/main.py
@@ -19,6 +19,7 @@ class DiscrimativeModel():
         self.lng = self.data[:, 33]
 
         self.graph = np.zeros([len(self.idx), len(self.idx)])
+        self.W = np.zeros([len(self.idx), len(self.idx)])
         for i in range(len(self.idx)):
             for j in range(len(self.idx)):
                 if i != j:
@@ -27,24 +28,25 @@ class DiscrimativeModel():
                         math.cos(self.lat[i]) * math.cos(self.lat[j]) +
                         math.sin(self.lat[i]) * math.sin(self.lat[j])
                     )
-
-    def fit_data(self, t, i):
-        t_temp = np.concatenate([t[:i], t[i + 1:]])
-        # lat_temp = np.concatenate([self.lat[:i], self.lat[i + 1:]])
-        # lng_temp = np.concatenate([self.lng[:i], self.lng[i + 1:]])
-        # lr = self.fit_data(t_temp, lat_temp, lng_temp)
-        # t_pred = lr.predict(np.array([self.lat[i], self.lng[i]]).reshape(1, -1))
-
-        dist = np.concatenate([self.graph[i, :i], self.graph[i, i + 1:]])
-        dist_down = 1 / dist
-        prob = dist_down / np.sum(dist_down)
-        # prob = torch.softmax(torch.tensor(dist_down), dim=0).numpy()
-        t_pred = np.sum(t_temp * prob)
-
-        return t_pred
+        self.W = np.exp(-self.graph ** 2 / 2 / 5 ** 2)
 
 
-    def detect_outlier(self, t, lat=None, lng=None):
+    def fit_data(self, t, i, day):
+        # t_temp = np.concatenate([t[:i], t[i + 1:]])
+        # dist = torch.from_numpy(np.concatenate([self.graph[i, :i], self.graph[i, i + 1:]]))
+        # k = 149
+        # dist_mink, idx_mink = torch.topk(dist, k, largest=False)
+        # dist_down = 1 / dist_mink.numpy()
+        # prob = dist_down / np.sum(dist_down)
+        # t_pred = np.sum(t_temp[idx_mink] * prob)
+        # return t_pred - t[i]
+
+        local_var_loss = 0
+        for j in range(len(self.idx)):
+            local_var_loss += (t[j] - t[i]) ** 2 * self.W[i, j]
+        return local_var_loss        
+
+    def detect_outlier(self, t, day, lat=None, lng=None):
         if lat is None:
             lat = self.lat
         if lng is None:
@@ -55,9 +57,9 @@ class DiscrimativeModel():
 
         delta = np.zeros(t.shape)
         for i in range(lat.shape[0]):
-            t_pred = self.fit_data(t, i)
-            delta[i] = t_pred - t[i]
-
+            delta_pred = self.fit_data(t, i, day)
+            delta[i] = delta_pred
+        
         pred = np.argmax(np.abs(delta))
 
         return pred
@@ -67,12 +69,20 @@ if __name__ == '__main__':
     path = '../data.mat'
     model = DiscrimativeModel(path)
     preds = np.zeros([150, 31])
-    t = copy.deepcopy(model.treal)
-    for i in range(150):
-        for j in range(31):
-            t[i, j] += 20
-            preds[i, j] = model.detect_outlier(t[:, j])
-            t[i, j] -= 20
     labels = np.arange(150).repeat(31).reshape(150, 31)
+    t = copy.deepcopy(model.treal)
+    delta_t = 20
+    front = 28
+    alpha = 0.5
+    for j in range(31):
+        for i in range(150):
+            t[i, j] += delta_t
+            if j > 0 and t[i, j] - np.average(t[i, max(j - front, 0):j]) > delta_t * alpha:
+                preds[i, j] = preds[i, j] = model.detect_outlier(t[:, j], j) # i
+            else:
+                preds[i, j] = model.detect_outlier(t[:, j], j)
+            t[i, j] -= delta_t
+        acc_j = np.sum(labels[:, j] == preds[:, j]) / 150
+        print(f'{j}: acc = {acc_j}')
     acc = np.sum(labels == preds) / 4650
-    print(acc)
+    print(f'total: acc = {acc}')
