45b7f5a71b7d0c2ef7425cebe78671bd4c10c806
TCL <1620508360@qq.com>
2023-01-12 00:02:30 +0800

    chore: add final version

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class DiscriminativeModel():
             local_var += (t[j] - t[i]) ** 2 * self.W[i, j]
         return local_var        
 
-    def detect_outlier(self, t, day, lat=None, lng=None):
+    def analyze_space(self, t, day, lat=None, lng=None):
         if lat is None:
             lat = self.lat
         if lng is None:
@@ -47,24 +47,37 @@ class DiscriminativeModel():
         pred = np.argmax(np.abs(delta))
 
         return pred
-
+    
+    def analyze_time(self, t, day, threhold):
+        delta = t[:, day] - np.average(t[:, max(day - front, 0): day])
+        pred = np.argmax(np.abs(delta))
+        if abs(delta[pred]) >= threhold:
+            return pred
+        else: 
+            return -1
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=str, default='./data.mat')
     parser.add_argument('--delta_t', type=int, default=20)
-    parser.add_argument('--front', type=int, default=28)
-    parser.add_argument('--alpha', type=float, default=0.5)
+    parser.add_argument('--front', type=int, default=10)
+    parser.add_argument('--threhold', type=float, default=50)
     parser.add_argument('--use_time_info', action='store_true')
     parser.add_argument('--sigma', type=float, default=3.0)
+    parser.add_argument('--seed', type=int, default=12345)
+    parser.add_argument('--rand_add', action='store_true', help="whether to randomly changing temperature")
     args = parser.parse_args()
 
     path = args.path
     delta_t = args.delta_t
     front = args.front
-    alpha = args.alpha
+    threhold = args.threhold
     use_time_info = args.use_time_info
     sigma = args.sigma
+    seed = args.seed
+    random_add = args.rand_add
+
+    np.random.seed(seed)
 
     model = DiscriminativeModel(path, sigma)
     preds = np.zeros([150, 31])
@@ -72,15 +85,23 @@ if __name__ == '__main__':
     t = copy.deepcopy(model.treal)
     for j in range(31):
         for i in range(150):
-            t[i, j] += delta_t
-            if j > 0 and t[i, j] - np.average(t[i, max(j - front, 0):j]) > delta_t * alpha:
-                if use_time_info:
-                    preds[i, j] = i
-                else:
-                    preds[i, j] = preds[i, j] = model.detect_outlier(t[:, j], j)
+            if random_add:
+                pm = 1 if np.random.randint(0, 2) == 0 else -1
             else:
-                preds[i, j] = model.detect_outlier(t[:, j], j)
-            t[i, j] -= delta_t
+                pm = 1
+            t[i, j] += delta_t * pm
+            if j >= front:
+                if use_time_info:
+                    time_predict = model.analyze_time(t, j, threhold)
+                    if time_predict == -1:
+                        preds[i, j] = model.analyze_space(t[:, j], j)
+                    else:
+                        preds[i, j] = time_predict
+                else:
+                    preds[i, j] = model.analyze_space(t[:, j], j)
+            else:
+                preds[i, j] = model.analyze_space(t[:, j], j)
+            t[i, j] -= delta_t * pm
         acc_j = np.sum(labels[:, j] == preds[:, j]) / 150
         print(f'{j}: acc = {acc_j}')
     acc = np.sum(labels == preds) / 4650
